9e6423abccc3f1db4168247024288af5a697799c
Akihito759 <GregCudok@yahoo.co.uk>
2016-11-21 22:00:22 +0100

    Connect to database

--- php/connect_sql.php
+++ php/connect_sql.php
@@ -1,6 +1,6 @@
 <?php
 	$host= "localhost";
-	$db_user ="root";
-	$db_password ="";
-	$db_name = "pwm";
+	$db_user ="id226011_pwm_admin";
+	$db_password ="pilot203";
+	$db_name = "id226011_pwm";
 ?>
